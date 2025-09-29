9ea16225eab389cc3c6028a09d8a03db2e8bed5b
hmj2422021 <hmj2422021@stu.o-hara.ac.jp>
2025-09-29 19:56:57 +0900

    射撃の追加

--- ReRoom/Assets/Player/PlayerController.cs
+++ ReRoom/Assets/Player/PlayerController.cs
@@ -1,12 +1,13 @@
 using Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float m_moveSpeed;         //�ړ����x
-    [SerializeField] float m_jumpPower;         //�W�����v��
-    [SerializeField] AudioClip m_bulletShot;    //�ˌ���
+    [SerializeField] float m_moveSpeed;         //移動速度
+    [SerializeField] float m_jumpPower;         //ジャンプ力
+    [SerializeField] AudioClip m_bulletShot;    //射撃音
 
     private CharacterController m_characterController;
     private PlayerInput m_playerInput;
@@ -15,18 +16,16 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
-        //�R���|�[�l���g�̎擾
-        m_characterController = GetComponent<CharacterController>();
-        m_playerInput = GetComponent<PlayerInput>();
-
-        //�J�[�\���̔�\��
+        //カーソルを消して中央に固定
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        m_characterController = GetComponent<CharacterController>();
+        m_playerInput = GetComponent<PlayerInput>();
     }
 
     private void OnEnable()
     {
-        //���͎��̃C�x���g��ݒ�
         m_playerInput.actions["Move"].performed += OnMove;
         m_playerInput.actions["Move"].canceled += OnMoveCancel;
 
@@ -37,7 +36,6 @@ public class PlayerController : MonoBehaviour
 
     private void OnDisable()
     {
-        //�ݒ肵���C�x���g�̏��O
         m_playerInput.actions["Move"].performed -= OnMove;
         m_playerInput.actions["Move"].canceled -= OnMoveCancel;
 
@@ -48,59 +46,47 @@ public class PlayerController : MonoBehaviour
 
     private void OnMove(InputAction.CallbackContext context)
     {
-        //�ړ��ʂ̎擾
         Vector2 input = context.ReadValue<Vector2>();
         m_inputValue = new Vector3(input.x, m_inputValue.y, input.y);
     }
 
     private void OnMoveCancel(InputAction.CallbackContext context)
     {
-        //�ړ��ʂ��Ȃ���
         m_inputValue = Vector3.zero;
     }
 
     private void OnJump(InputAction.CallbackContext context)
     {
-        //�ڒn���Ă���Ώ�����ɑ��x��^����
         if (!m_characterController.isGrounded) return;
         m_inputValue.y = m_jumpPower;
     }
 
     private void OnShot(InputAction.CallbackContext context)
     {
-        //�ˌ����̍Đ�
         SoundManager.Play2D(m_bulletShot);
 
-        RaycastHit hit; 
-        Ray ray = new Ray(transform.position, Camera.main.transform.position);
-
-        //�e���̐���
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        //カメラの中央からRayを飛ばす
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out var hit))
         {
-
+            Debug.Log(hit.transform.gameObject.name.ToString());
         }
     }
 
     private void FixedUpdate()
     {
-        //�ړ�
         Move();
 
-        //���R����
         m_inputValue.y += Physics.gravity.y * Time.deltaTime;
     }
 
     private void Move()
     {
-        //�J�����̌������l�������ړ���
         Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
         Vector3 moveVelocity = cameraForward * m_inputValue.z + Camera.main.transform.right * m_inputValue.x;
         moveVelocity = new Vector3(moveVelocity.x * m_moveSpeed, m_inputValue.y, moveVelocity.z * m_moveSpeed);
 
-        //�ړ�
         m_characterController.Move(moveVelocity * Time.deltaTime);
 
-        //�ړ����Ă���Ή�]������
         Vector3 move = new Vector3(m_inputValue.x, 0, m_inputValue.z);
         if (move != Vector3.zero)
         {
